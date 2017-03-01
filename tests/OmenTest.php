d1c949add95ca05c6d35b12e34a96cce7effc955
Zazalt <liviu+zazalt+github.com@sindla.com>
2017-03-02 01:43:47 +0200

    The cake is a lie.

--- tests/OmenTest.php
+++ tests/OmenTest.php
@@ -6,10 +6,22 @@ use Zazalt\Omen\Omen;
 
 class OmenTest extends \Zazalt\Omen\Tests\ZazaltTest
 {
+    /** @var Omen */
     protected $that;
 
     public function __construct()
     {
         parent::loader(Omen::class, []);
+    }
+
+    public function testContentToString(): void
+    {
+        $batteryTest = [
+            "test = 'test'" => ['test', '=', 'test']
+        ];
+
+        foreach ($batteryTest as [$expected, $given]) {
+            $this->assertEquals($expected, $this->that->contentToString($given));
+        }
     }
 }
